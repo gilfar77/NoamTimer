645dd4780ffd41208c29dba8529a5200d05abf11
Gil Fartook <gilf@corp.credito.co.il>
2024-06-19 21:51:34 +0300

    Fix endpoint and ensure JSON initialization

--- server.js
+++ server.js
@@ -8,18 +8,20 @@ const PORT = process.env.PORT || 3000;
 const dataFilePath = path.join(__dirname, 'data.json');
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Function to initialize data.json with an initial date if it doesn't exist
+function initializeJsonFile() {
+    if (!fs.existsSync(dataFilePath)) {
+        const initialDate = new Date('June 10, 2024 16:00:00').getTime();
+        saveLastMeetingDate(initialDate);
+    }
+}
 
 // Load last meeting date from JSON file
 function loadLastMeetingDate() {
-    if (fs.existsSync(dataFilePath)) {
-        const data = fs.readFileSync(dataFilePath);
-        return JSON.parse(data).lastMeetingDate;
-    } else {
-        const initialDate = new Date('June 10, 2024 16:00:00').getTime();
-        saveLastMeetingDate(initialDate);
-        return initialDate;
-    }
+    const data = fs.readFileSync(dataFilePath);
+    return JSON.parse(data).lastMeetingDate;
 }
 
 // Save last meeting date to JSON file
@@ -27,6 +29,9 @@ function saveLastMeetingDate(date) {
     const data = { lastMeetingDate: date };
     fs.writeFileSync(dataFilePath, JSON.stringify(data));
 }
+
+// Initialize the JSON file
+initializeJsonFile();
 
 app.get('/lastMeetingDate', (req, res) => {
     const lastMeetingDate = loadLastMeetingDate();
