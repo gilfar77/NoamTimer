442e6a6b9c764c63c71f47df89b4018b0e1b529e
Gil Fartook <gilf@corp.credito.co.il>
2024-06-19 21:38:08 +0300

    Switch to JSON file for storage

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-    db.run("CREATE TABLE meeting (id INTEGER PRIMARY KEY, lastMeetingDate INTEGER)");
-    db.run("INSERT INTO meeting (lastMeetingDate) VALUES (?)", [new Date('June 10, 2024 16:00:00').getTime()]);
-});
+const dataFilePath = path.join(__dirname, 'data.json');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// Load last meeting date from JSON file
+function loadLastMeetingDate() {
+    if (fs.existsSync(dataFilePath)) {
+        const data = fs.readFileSync(dataFilePath);
+        return JSON.parse(data).lastMeetingDate;
+    } else {
+        const initialDate = new Date('June 10, 2024 16:00:00').getTime();
+        saveLastMeetingDate(initialDate);
+        return initialDate;
+    }
+}
+
+// Save last meeting date to JSON file
+function saveLastMeetingDate(date) {
+    const data = { lastMeetingDate: date };
+    fs.writeFileSync(dataFilePath, JSON.stringify(data));
+}
+
 app.get('/lastMeetingDate', (req, res) => {
-    db.get("SELECT lastMeetingDate FROM meeting WHERE id = 1", (err, row) => {
-        if (err) {
-            return res.status(500).json({error: err.message});
-        }
-        res.json({lastMeetingDate: row.lastMeetingDate});
-    });
+    const lastMeetingDate = loadLastMeetingDate();
+    res.json({ lastMeetingDate });
 });
 
 app.post('/updateMeetingDate', (req, res) => {
     const { lastMeetingDate } = req.body;
-    db.run("UPDATE meeting SET lastMeetingDate = ? WHERE id = 1", [lastMeetingDate], function(err) {
-        if (err) {
-            return res.status(500).json({error: err.message});
-        }
-        res.json({ message: 'Date updated successfully' });
-    });
+    saveLastMeetingDate(lastMeetingDate);
+    res.json({ message: 'Date updated successfully' });
 });
 
 app.listen(PORT, () => {
